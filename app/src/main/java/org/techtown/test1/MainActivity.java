9725236e9bd615e229a4751fa3271dd529d2213d
Sangho Choe <sangho.choe@gmail.com>
2022-02-09 16:25:37 +0900

    first commit

---
+++ app/src/main/java/org/techtown/test1/MainActivity.java
@@ -0,0 +1,20 @@
+package org.techtown.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /* String hello = new String() */
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
